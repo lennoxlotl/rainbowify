299613085f8254d2093953180e7208ac85f7ee20
Lennox <lennox2508@gmail.com>
2021-07-26 06:23:16 +0200

    Replace guava supplier with java supplier

--- src/main/java/de/lennox/rainbowify/gl/GLUtil.java
+++ src/main/java/de/lennox/rainbowify/gl/GLUtil.java
@@ -18,12 +18,16 @@
  */
 package de.lennox.rainbowify.gl;
 
-import com.google.common.base.Supplier;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.*;
+import net.minecraft.client.render.Shader;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Matrix4f;
+
+import java.util.function.Supplier;
 
 public class GLUtil {
 
