ed2d93e951a3e91b2ce4059aaed1c7302a2edf24
officialLennox <lennox2508@gmail.com>
2023-06-08 17:09:19 +0200

    feat: add missing header

---
+++ src/main/java/de/lennox/rainbowify/event/events/DrawWorldEvent.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021-2023 Lennox
+ *
+ * This file is part of rainbowify.
+ *
+ * rainbowify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rainbowify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rainbowify.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.lennox.rainbowify.event.events;
+
+import de.lennox.rainbowify.event.Event;
+
+/**
+ * Called on world draw (3d)
+ *
+ * @since 2.0.0
+ * @author Lennox
+ */
+public class DrawWorldEvent extends Event {
+}
